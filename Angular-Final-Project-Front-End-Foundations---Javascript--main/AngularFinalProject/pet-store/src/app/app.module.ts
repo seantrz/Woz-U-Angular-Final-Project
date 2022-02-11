c0ba723fadf9109adec3eaa4c2ebddee55d8edcc
Sean <74019460+seantrz@users.noreply.github.com>
2022-02-11 10:23:08 -0800

    Add files via upload
    Create a new Angular application from scratch using the Angular CLI tool.
    
    This project will allow users to create and manage tasks using Read and Delete:
    
    A user should be able to view a list of tasks by their name.
    
    A user should be able to delete a specific task on the list of tasks page.
    
    Use the same JSON server to serve up this file.
    
    This will allow you to use HTTP verbs to perform Read and Delete operations.

---
+++ Angular-Final-Project-Front-End-Foundations---Javascript--main/AngularFinalProject/pet-store/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PetListComponent } from "./pet-list/pet-list.component";
+import { PetAddComponent } from "./pet-add/pet-add.component";
+import { NavComponent } from "./nav/nav.component";
+import { PetEditComponent } from './pet-edit/pet-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PetListComponent,
+    PetAddComponent,
+    NavComponent,
+    PetEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
